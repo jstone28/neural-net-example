525a83e106b66be20688d27161a0046080e1505b
J.Stone <jameson.stone@itential.com>
2018-10-07 16:48:39 -0400

    :art: add print lines for tracking and improve readability

--- NeuralNet.py
+++ NeuralNet.py
@@ -17,6 +17,7 @@ class NeuralNetwork:
   def feedforward(self):
     self.layer1 = sigmoid(np.dot(self.input, self.weights1))
     self.output = sigmoid(np.dot(self.layer1, self.weights2))
+    print(nm.output)
 
   def backprop(self):
     d_weights2 = np.dot(self.layer1.T, (2 * (self.y - self.output) * sigmoid_derivative(self.output)))
@@ -24,16 +25,24 @@ class NeuralNetwork:
 
     self.weights1 += d_weights1
     self.weights2 += d_weights2
+    print(nm.output)
 
 if __name__ == "__main__":
-  X = np.array([[0, 0, 1],
-               [0, 1, 1],
-               [1, 0, 1],
-               [1, 1, 1]])
-  y = np.array([[0], [1], [1], [0]])
+  X = np.array([
+                [0, 0, 1],
+                [0, 1, 1],
+                [1, 0, 1],
+                [1, 1, 1]
+              ])
+  y = np.array([
+                [0],
+                [1],
+                [1],
+                [0]
+              ])
   nm = NeuralNetwork(X, y)
 
-  for i in range(1500):
+  for i in range(15000):
     nm.feedforward()
     nm.backprop()
 
